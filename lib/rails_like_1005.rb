541014e3252d53466c537d2b9e86b50fd24605a8
peck liu <xiaoyang20170204@gmail.com>
2017-10-05 18:08:33 +0800

    add debug

--- lib/rails_like_1005.rb
+++ lib/rails_like_1005.rb
@@ -3,6 +3,7 @@ require "rails_like_1005/version"
 module RailsLike1005
   class Application
     def call(env)
+       `echo debug > debug.txt`;
       [200, {'Content-Type' => 'text/html'},
         ["Hello from Ruby on RailsLike1005!"]]
     end
